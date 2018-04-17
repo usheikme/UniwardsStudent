97c87ebcf007547f4ec40f09a57c23c0140f642b
BuildTools <unconfigured@null.spigotmc.org>
2018-04-17 16:42:29 +0400

    Made edittext round and small fixes

--- app/src/main/java/xyz/uniwards/uniwards_student/LoginActivity.java
+++ app/src/main/java/xyz/uniwards/uniwards_student/LoginActivity.java
@@ -1,6 +1,7 @@
 package xyz.uniwards.uniwards_student;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
@@ -23,13 +25,23 @@ public class LoginActivity extends AppCompatActivity {
 
     //Initialize the onclicklistener for the login button and make the button send the login request on click
     protected void InitLoginButton() {
-        Toast.makeText(getBaseContext(), "FFF", Toast.LENGTH_LONG).show();;
+        Toast.makeText(getBaseContext(), "FFF", Toast.LENGTH_LONG).show();
         Button loginButton = findViewById(R.id.button_login);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //Toast.makeText(getApplicationContext(), "GG", Toast.LENGTH_LONG).show();;
                 new AsyncLogin(LoginActivity.this).execute();
+            }
+        });
+    }
+
+    protected void InitRegisterAsClickable() {
+        TextView text_register = findViewById(R.id.text_register);
+        text_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent();
             }
         });
     }
